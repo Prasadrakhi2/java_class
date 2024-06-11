5ffcb1b67cc8549f7fbb656a7051cff921ce3756
rakhi prasad <142569721+Prasadrakhi2@users.noreply.github.com>
2024-06-11 05:13:58 +0000

    deleat Node in bst

--- 20_BinaryTree/_11Bst.java
+++ 20_BinaryTree/_11Bst.java
@@ -56,6 +56,52 @@ public class _11Bst{
         }
     }
 
+
+    /*deleat function */
+    /* case 1 -> leaf node
+     * case 2 -> single child
+     * case 3 -> 2 child
+     */
+
+     public static Node deleat(Node root, int val){
+        if(root.data > val){
+            root.left = deleat(root.left, val);
+        }
+        
+        else if(root.data < val){
+            root.right = deleat(root.right, val);
+        }
+
+        else{
+            //case 1
+            if(root.left == null && root.right == null){
+                return null;
+            }
+
+            //case 2
+            if(root.left == null){
+                return root.right;
+            }
+            else if(root.right == null){
+                return root.left;
+            }
+
+            //case 3
+                Node IS = findInorderSucessor(root.right);
+                root.data = IS.data;
+                root.right = deleat(root.right, IS.data);
+        }
+
+        return root;
+     }
+
+     public static Node findInorderSucessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+     }
+
     public static void main(String args[]){
 
         /*
@@ -68,14 +114,14 @@ public class _11Bst{
                         2   4
          */
 
-        int val[] = {5,1,3,4,2,7};
+        int val[] = {8,5,3,1,4,6,10,11,14};
         Node root = null;
 
         for(int i=0; i<val.length; i++){
             root = insert(root, val[i]);
         }
 
-        inorder(root);
+        /*inorder(root);
         System.out.println();
 
         if(search(root, 6)){
@@ -83,6 +129,16 @@ public class _11Bst{
         }
         else{
             System.out.println("key is not found");
-        }
+        }*/
+
+
+        //deleat node 
+        inorder(root);
+        System.out.println();
+
+        root = deleat(root, 5);
+        System.out.println();
+        inorder(root);
+        System.out.println();
     }
 }
